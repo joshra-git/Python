f44d4c6476180b432d573f2010d78d680dce53a9
Joshua Brown <joshua.brown@n-able.com>
2023-07-19 12:50:44 +0200

    saving

---
+++ Day 1/day1.py
@@ -0,0 +1,7 @@
+# the LEN takes the length of something and will show how many characters it is
+# https://note.nkmk.me/en/python-str-len/#:~:text=()%20in%20Python-,Get%20the%20length%20of%20a%20string%20(number%20of%20characters)%20with,returned%20as%20an%20integer%20value.
+
+var = input("What is your name? ")
+
+print(len(var))
+
