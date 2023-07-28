425ddb189a785212895812d3915a0a8a09ec2c2e
Joshua Brown <joshra@gmail.com>
2023-07-28 05:16:29 +0000

    pushing

---
+++ Day_8/day-8-start.py
@@ -0,0 +1,7 @@
+def hello(name="Josh",location="Poland"):
+    print(f"Hello {name} in {location}")
+    print("Hello")
+    print("there")
+
+
+hello()
---
+++ Day_8/paint.py
@@ -0,0 +1,11 @@
+
+
+
+restart = True
+
+while restart == True:
+    restart = str(input("Do you want to restart:\n").lower())
+    if restart == "yes":
+        restart = True
+    elif restart == "no":
+        restart = False
