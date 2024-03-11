35c87c03897c5b7cb8099028a0368930eaf4f7d4
Joshua Brown <joshra@gmail.com>
2024-03-11 07:18:23 +0000

    adding in more

--- Day_16/coffee/main.py
+++ Day_16/coffee/main.py
@@ -8,3 +8,26 @@ from money_machine import MoneyMachine
 # 3. Process Coins
 # 4. Check transaction successsful?
 # 5. Make Coffee
+ordering = Menu()
+zloty = MoneyMachine()
+drinky_poo = CoffeeMaker()
+
+# Gets the order of which I want
+c_order = input("What would you like? latte, esspresso or cappuccino?: ")
+
+# Feeds that order in to ensure it is actually an option to be able to select
+drinky_poo = ordering.find_drink(c_order)
+
+# Gets the items that are required in order to make it
+cost = ordering.get_items()
+
+
+
+asdf = ordering.menu
+
+print (asdf)
+
+# zloty.process_coins(dollary)
+
+# zloty.make_payment(dollary)
+
